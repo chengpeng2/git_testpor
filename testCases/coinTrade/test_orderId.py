6c50efde3700190d43095a29efa6e40a4638d502
1991789649@qq.com <1991789649@qq.com>
2019-09-08 23:16:38 +0800

    git commit -m '取消下单'

--- testCases/coinTrade/test_orderId.py
+++ testCases/coinTrade/test_orderId.py
@@ -1,21 +1,22 @@
 import unittest
 import requests
 from common.readToken import get_token
-testData1=[{'orderId':'E224931084918333440','assert':'success'},{'orderId':'E224931084918333440','assert':'success'}]
+from common.readOrderId import get_orderId
+
 class Adds(unittest.TestCase):
     """取消下单"""
 
     def setUp(self):
-        self.headers = {"x-auth-token":"860dbe4f-1441-43c8-8f66-5390bc2bba79"}
+        self.headers = {"x-auth-token":get_token()}
 
 
     def tearDown(self):
         print('test over')
 
-    def test_adds1(self,value):
+    def test_adds1(self):
         """请求参数合法"""
         self.url = "https://api.ezbtest.top/exchange/order/cancel/"
-        res = requests.post(self.url+'value[]', headers=self.headers).json()
+        res = requests.post(self.url+get_orderId(), headers=self.headers).json()
         print(res)
         self.assertIn('success', res['message'])
 
